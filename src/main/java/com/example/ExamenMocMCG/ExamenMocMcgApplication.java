68624c64280c746f5407d75e791f1da096a6df25
Marta Cortina Gonzalez <martacg15@educastur.es>
2025-11-21 17:33:48 +0100

    Initial commit

---
+++ src/main/java/com/example/ExamenMocMCG/ExamenMocMcgApplication.java
@@ -0,0 +1,13 @@
+package com.example.ExamenMocMCG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenMocMcgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenMocMcgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ExamenMocMCG/ExamenMocMcgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ExamenMocMCG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenMocMcgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
